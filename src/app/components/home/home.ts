4487b66a3473eebb3f13d23b99b4781375b8b155
pablo moreno <pablommoreno5@gmail.com>
2026-05-26 17:23:40 -0300

    quinto sprint

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
-import { RouterLink,Router, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+import { RouterLink, Router, RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { AuthServise } from '../../services/auth';
 import { effect } from "@angular/core";
 import { UserServices } from '../../services/usuarioService';
@@ -13,23 +13,36 @@ import { QuienSoy } from "../quien-soy/quien-soy";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
 
 
   private auth = inject(AuthServise);
   email = '';
   mostrar = signal(false);
-  mostrarMenu= signal(false);
+  mostrarMenu = signal(false);
+  isAdmin = signal(false);
 
   constructor(public router: Router) {
 
     effect(() => {
 
-      this.email = this.auth.userMail(); 
+      this.email = this.auth.userMail();
 
     })
   }
 
+
+  async ngOnInit() {
+    const id = this.auth.user()?.id;
+
+    if (id) {
+
+      const admin = await this.auth.esAdmin(id);
+
+      this.isAdmin.set(admin);
+
+    }
+  }
 
   cerrarSesion() {
     this.auth.logOut()
